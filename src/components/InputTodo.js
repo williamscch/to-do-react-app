382749118ea1fbedac485d9d087e56a126303585
wdavidcch <david.13.2121@gmail.com>
2022-08-08 17:01:07 -0400

    Add functionalities and components

---
+++ src/components/InputTodo.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react"
+
+class InputTodo extends Component {
+    state = {
+      title: ""  
+    };
+
+    handleSubmit = e => {
+        e.preventDefault()
+        if (this.state.title.trim()) {
+          this.props.addTodoProps(this.state.title)
+          this.setState({
+            title: "",
+          })
+        } else {
+          alert("Please write item")
+        }
+      }
+
+    onChange = e => {
+        this.setState({
+          title: e.target.value
+        });
+      };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Add Todo..." value={this.state.title} onChange={this.onChange}/>
+        <button>Submit</button>
+      </form>
+    )
+  }
+}
+export default InputTodo
