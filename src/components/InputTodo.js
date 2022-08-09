398ca1af650c1cd5b6024137de28bb48995b95a7
wdavidcch <david.13.2121@gmail.com>
2022-08-09 11:04:59 -0400

    Update Readme and add MIT.md

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -24,9 +24,16 @@ class InputTodo extends Component {
       };
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input type="text" placeholder="Add Todo..." value={this.state.title} onChange={this.onChange}/>
-        <button>Submit</button>
+        <form onSubmit={this.handleSubmit} className="form-container">
+        <input
+          type="text"
+          className="input-text"
+          placeholder="Add todo..."
+          value={this.state.title}
+          name="title"
+          onChange={this.onChange}
+        />
+        <button className="input-submit">Submit</button>
       </form>
     )
   }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react"
 import ReactDOM from "react-dom"
 //component file
 import TodoContainer from "./components/TodoContainer"
+//stylesheet
+import "./App.css"
+
 ReactDOM.render(
   <React.StrictMode>
     <TodoContainer />
